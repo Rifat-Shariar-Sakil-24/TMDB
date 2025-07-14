7633abce67f101eb89dbb69b98b6d9652f670785
Rifat Shariar Sakil <shariarsakil101@gmail.com>
2025-07-14 23:58:27 +0600

    Fix Proxy File Issue in Production

--- src/app/core/constants/api.config.ts
+++ src/app/core/constants/api.config.ts
@@ -1,6 +1,14 @@
+import { environment } from './../../../environments/environment';
 
+const API_BASE_URL = environment.production
+  ? 'https://api.themoviedb.org/3'
+  : '';
 
 export const ApiEndpoints = {
-  movieListBaseURL: `/discover/movie?sort_by=popularity.desc`,
-  movieDetails: (movieId: string | number) => `/movie/${movieId}?append_to_response=credits`,
+  movieListBaseURL: `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`,
+  movieDetails: (movieId: string | number) =>
+    `${API_BASE_URL}/movie/${movieId}?append_to_response=credits`,
 };
+
+
+
