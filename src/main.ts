e2cfd9292eb5b547296661fcd5b8c67da774bd3a
Rifat Sakil. Shahriar <sakil@kaz.com.bd>
2025-07-14 13:51:39 +0600

    Hide Console Logs in Production

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  
+  console.log = () => {};
+  console.warn = () => {};
+  console.info = () => {};
+  console.debug = () => {};
+
+  //console.error = () => {};
+}
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
