6967685190a40e81f40fa08061c29e12e79c7dbf
Rifat Sakil. Shahriar <sakil@kaz.com.bd>
2025-07-17 12:45:08 +0600

    Fix Search Box navigation from Banner

--- src/app/pages/home/components/banner/banner.component.ts
+++ src/app/pages/home/components/banner/banner.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './banner.component.html',
-  styleUrl: './banner.component.css'
+  styleUrls: ['./banner.component.css']
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit {
 
+  searchQuery: string = '';
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    console.log("hell");
+  }
+
+  onSearch(event: Event) {
+    event.preventDefault();
+    console.log("Search triggered with:", this.searchQuery);
+    if (this.searchQuery.trim()) {
+      const term = this.searchQuery.trim();
+      this.router.navigate(['/search/movie'], { queryParams: { query: term } });
+    }
+  }
 }
+
+
